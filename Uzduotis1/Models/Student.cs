9a1649af3641920ef4abda3dd3d0d65a8110b372
Sarunas <sarugen12@gmail.com>
2025-02-26 13:25:40 +0200

    uzduotis su klasemis 1 prideta (studnet klase)

---
+++ Uzduotis1/Models/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uzduotis1.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int[] Grades { get; set; }
+
+        public double GetAverageGrade()
+        {
+            double sum = 0;
+
+            for(int i = 0; i < Grades.Length; i++)
+            {
+                sum += Grades[i];
+            }
+
+            return sum / Grades.Length;
+        }
+        public string GetStatus()
+        {
+            string result = string.Empty;
+            double average = GetAverageGrade();
+            if (average >= 9)
+                result = "Puiku";
+            else if (average >= 7)
+                result = "Gerai";
+            else if (average >= 5)
+                result = "Patenkinama";
+            else
+                result = "Nepatenkinama";
+
+            return result;
+        }
+    }
+}
---
+++ Uzduotis1/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * 1.Studentų Pažymiai
+Sukurkite klasę Student, kuri turi šiuos laukus:
+Name (studento vardas),
+Grades (masyvas su studento pažymiais, pvz., [8, 9, 7, 10]).
+Parašykite metodą GetAverageGrade(), kuris apskaičiuoja ir grąžina studento pažymių vidurkį.
+Parašykite metodą GetStatus(), kuris pagal vidurkį grąžina:
+"Puiku", jei vidurkis >= 9,
+"Gerai", jei vidurkis >= 7,
+"Patenkinama", jei vidurkis >= 5,
+"Nepatenkinama", jei vidurkis < 5.
+Sukurkite bent 3 studentus ir išveskite jų vardus, vidurkius bei statusą.
+ */
+
+using System;
+using Uzduotis1.Models;
+
+namespace Uzduotis1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Kiek studentu noresite prideti? ");
+            int sCount = int.Parse(Console.ReadLine());
+            Student[] students = new Student[sCount];
+            for(int i = 0; i < sCount; i++)
+            {
+                Console.WriteLine("Studento Vardas: ");
+                string sName = Console.ReadLine();
+
+                Console.WriteLine("Kiek pazymiu ivesite: ");
+                int gCount = int.Parse(Console.ReadLine());
+                int[] sGrades = new int[gCount];
+                for(int j = 0; j < gCount; j++)
+                {
+                    Console.WriteLine($"Iveskite {j + 1} studento {sName} pazymi: ");
+                    sGrades[j] = int.Parse(Console.ReadLine());
+                }
+
+                students[i] = new Student { Name = sName, Grades = sGrades };
+            }
+
+            foreach(Student s in students)
+            {
+                Console.WriteLine($"Studento {s.Name} statusas: "+s.GetStatus());
+            }
+        }
+    }
+}
